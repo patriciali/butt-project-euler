2c85038900f2633d76d5a4c485682baee6f316ea
Patricia Li <patricia.li@gmail.com>
2021-08-31 21:34:26 -0700

    god damn it

--- 6.hs
+++ 6.hs
@@ -1,10 +1,2 @@
-reduce :: [Int] -> (Int -> Int -> Int) -> Int -> Int
-reduce (x:[]) reducer accumulator = reducer accumulator x
-reduce (x:xs) reducer accumulator = reduce xs reducer (reducer accumulator x)
-
-addElements :: [Int] -> Int
-addElements xs = reduce xs (+) 0
-
 main = do
-    let nums = [1, 2 .. 100]
-    print ((addElements nums)^2 - addElements [x^2 | x <- nums])
+    print (let nums = [1, 2 .. 100] in (sum nums)^2 - sum [x^2 | x <- nums])
