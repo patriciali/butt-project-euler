a377cbddf9a0d57904cdf823153253d2e38f89ef
Patricia Li <patricia.li@gmail.com>
2021-08-31 21:25:18 -0700

    this is crazy

--- 5.hs
+++ 5.hs
@@ -1,19 +1,18 @@
-reduce :: [Int] -> (Int -> Int -> Int) -> Int -> Int
-reduce (x:[]) reducer accumulator = reducer accumulator x
-reduce (x:xs) reducer accumulator = reduce xs reducer (reducer accumulator x)
-
-patriciaFind :: (a -> Bool) -> [a] -> a
-patriciaFind propertyFunc [] = error "no element found which satisfies the property"
-patriciaFind propertyFunc (x:xs)
-    | propertyFunc x = x
-    | otherwise = patriciaFind propertyFunc xs
-
-patriciaLcm :: Int -> Int -> Int
-patriciaLcm p q = patriciaFind isDivisibleByQ [p, 2*p ..]
-    where isDivisibleByQ x = x `mod` q == 0
-
 lcmList :: [Int] -> Int
-lcmList xs = reduce xs patriciaLcm 1
+lcmList xs = reduce xs lcm 1
+    where lcm p q = find isDivisibleByQ [p, 2*p ..]
+              where find :: (a -> Bool) -> [a] -> a
+                    find propertyFunc [] = error "no element found which satisfies the property"
+                    find propertyFunc (x:xs)
+                       | propertyFunc x = x
+                       | otherwise = find propertyFunc xs
+
+                    isDivisibleByQ :: Int -> Bool
+                    isDivisibleByQ x = x `mod` q == 0
+
+          reduce :: [Int] -> (Int -> Int -> Int) -> Int -> Int
+          reduce (x:[]) reducer accumulator = reducer accumulator x
+          reduce (x:xs) reducer accumulator = reduce xs reducer (reducer accumulator x)
 
 main = do
     print (lcmList [1, 2 .. 20])
